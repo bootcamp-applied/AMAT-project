f4f720c644790a3f2b21072d0480d18c06a411ee
myriam-Elkouby <essayagmyriam>
2023-07-18 16:55:01 +0300

    change of path

--- classification_project/study/main_myriam.py
+++ classification_project/study/main_myriam.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-from BL.preprocessing import Preprocessing
-from data_handler_cifar_10 import DataHandlerCifar10
-from data_handler_cifar_100 import DataHandlerCifar100
-from data_handler_data_handler_cifar_10_100import DataHandlerCifar10Cifar100
-from preprocessing import Preprocessing
-from visualization import Visualization
+from classification_project.data_handler.data_handler_cifar_10 import DataHandlerCifar10
+from classification_project.data_handler.data_handler_cifar_100 import DataHandlerCifar100
+from classification_project.data_handler.data_handler_cifar_10_100 import DataHandlerCifar10Cifar100
+from classification_project.preprocessing.preprocessing import Preprocessing
+from classification_project.visualization.visualization import Visualization
+from classification_project.utils.add_images import useDataNewImage
 import pandas as pd
 
 if __name__ == '__main__':
@@ -15,8 +15,9 @@ if __name__ == '__main__':
     cifar100.load_data_to_csv()
     cifar10_100=DataHandlerCifar10Cifar100()
     cifar10_100.load_data_to_csv()
-    data = pd.read_csv('../DAL/cifar_10_100_db.csv')
-    data = Preprocessing(data)
-    data.preparing_data()
-    x_train, y_train, x_val, y_val, x_test, y_test = data.split_to_train_test_validation()
-    print(x_train.shape, y_train.shape, x_val.shape, y_val.shape, x_test.shape, y_test.shape)
+    useDataNewImage()
+    # data = pd.read_csv('../DAL/cifar_10_100_db.csv')
+    # data = Preprocessing(data)
+    # data.preparing_data()
+    # x_train, y_train, x_val, y_val, x_test, y_test = data.split_to_train_test_validation()
+    # print(x_train.shape, y_train.shape, x_val.shape, y_val.shape, x_test.shape, y_test.shape)
