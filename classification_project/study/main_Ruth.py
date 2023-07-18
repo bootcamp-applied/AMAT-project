da2e1d33613f96f146e1e00e58fb4b93a25ab5fe
ruth-katzir <rutkatzir@gmail.com>
2023-07-18 20:18:40 +0300

    add_random_images

--- classification_project/study/main_Ruth.py
+++ classification_project/study/main_Ruth.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from classification_project.study.use_Visualization import Use_Visualization
-
+from classification_project.utils.add_imgs_cifar_100 import plot_images
 if __name__ == '__main__':
 
-    df = pd.read_csv('../../data/processed/cifar-10-100.csv')
+    #df = pd.read_csv('../../data/processed/cifar-10-100.csv')
     #pareto to df
-    Use_Visualization.pareto_to_df_label(df)
+    #Use_Visualization.pareto_to_df_label(df)
     #pareto to train_val_test
-    Use_Visualization.pareto_tarin_val_test(df)
+    #Use_Visualization.pareto_tarin_val_test(df)
+    plot_images()
 
 
