7628cc6c1a3ba98e952c696baae66933873d8151
ruth-katzir <rutkatzir@gmail.com>
2023-07-19 19:01:20 +0300

    before merge

--- classification_project/visualization/convergence_graphs.py
+++ classification_project/visualization/convergence_graphs.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from classification_project.visualization.visualization import Visualization
-
+from classification_project.study.use_Visualization import Use_Visualization
 # Generate random data for the example
 np.random.seed(42)
 X = np.random.rand(1000, 32, 32, 3)
@@ -37,6 +37,7 @@ Visualization.plot_precision_recall_curve(model, X_val, y_val)
 
 accuracy, loss, f1 = Visualization.calculate_validation_metrics(model, X_val, y_val)
 Visualization.plot_convergence_graphs(history)
+
 print("Validation Accuracy:", accuracy)
 print("Validation Loss:", loss)
 print("F1 Score:", f1)
