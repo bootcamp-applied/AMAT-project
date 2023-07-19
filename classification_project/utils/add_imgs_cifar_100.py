bea3afe66796cc2644c71aea744b6d907fcdfa06
ruth-katzir <rutkatzir@gmail.com>
2023-07-19 11:53:24 +0300

    before merge

--- classification_project/utils/add_imgs_cifar_100.py
+++ classification_project/utils/add_imgs_cifar_100.py
@@ -2,14 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_images():
-    # plotting the first img
-    df = pd.read_csv('../../data/processed/cifar-100.csv')
-    pixels = df.iloc[0, 2:]
-    image = pixels.values.reshape(3, 32, 32).transpose(1, 2, 0)
+def plot_images(images):
+    for image in images:
     plt.imshow(image)
     plt.show()
 
-    #get 5 random images
-    random_rows = df.sample(n=10)
-    #pixel_columns = random_rows.iloc[,2:]
+def load_new_images():
+    df = pd.read_csv('../../data/processed/cifar-100.csv')
+    #random_rows = df.sample(n=10)
+    no_labels=[10,11,12,13,14]
+    random_rows = df[~df['label'].isin(no_labels)].sample(n=10)
+    pixel_columns = random_rows.iloc[ :,2:]
+    images=[]
+    for _, row in pixel_columns.iterrows():
+        image = row.values.reshape(3, 32, 32).transpose(1, 2, 0)
+        images.append(image)
+
+
