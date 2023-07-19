e538d8879e21ebe1362a93ee11aa0709d2097254
Hadas-lab <hadasofek2@gmail.com>
2023-07-19 16:10:41 +0300

    addded demo df

---
+++ classification_project/utils/create_small_df.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+
+path_cifar_10 = '../../data/processed/cifar-10.csv'
+df_cifar_10 = pd.read_csv(path_cifar_10)
+
+new_df = df_cifar_10[:2000]
+path = '../../data/processed/small_df.csv'
+
+if os.path.exists(path):
+    os.remove(path)
+
+new_df.to_csv(path, index=False, encoding='utf-8', mode='w')
