91c60da6860d9010cea9036851885a175cfc6ab2
Hadas-lab <hadasofek2@gmail.com>
2023-07-20 16:16:50 +0300

    improve cnn to 80%

--- classification_project/models/cnn.py
+++ classification_project/models/cnn.py
@@ -4,55 +4,95 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.models import load_model as tf_load_model
 import joblib
 
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+
+
 class CNN:
-    def __init__(self):
-        # Defining the parameters
+    # def __init__(self, num_classes=10):
+    #     # Defining the parameters
+    #     self.batch_size = 32
+    #     self.num_classes = num_classes
+    #     self.epochs = 50
+    #     self.input_shape = (32, 32, 3)
+    #
+    #     # self.epochs = 5
+    #     # self.model = Sequential([
+    #     #     Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
+    #     #     MaxPooling2D((2, 2)),
+    #     #     Flatten(),
+    #     #     Dense(self.num_classes, activation='softmax')
+    #     # ])
+    #
+    #     self.model = Sequential()
+    #     self.model.add(Conv2D(64, (3, 3), padding='same',
+    #                           input_shape=self.input_shape))
+    #     self.model.add(Activation('relu'))
+    #     self.model.add(BatchNormalization())
+    #     self.model.add(Conv2D(64, (3, 3)))
+    #     self.model.add(Activation('relu'))
+    #     self.model.add(BatchNormalization())
+    #     self.model.add(MaxPooling2D(pool_size=(2, 2)))
+    #     self.model.add(Dropout(0.25))
+    #
+    #     self.model.add(Conv2D(128, (3, 3), padding='same'))
+    #     self.model.add(Activation('relu'))
+    #     self.model.add(BatchNormalization())
+    #     self.model.add(Conv2D(128, (3, 3)))
+    #     self.model.add(Activation('relu'))
+    #     self.model.add(BatchNormalization())
+    #     self.model.add(MaxPooling2D(pool_size=(2, 2)))
+    #     self.model.add(Dropout(0.25))
+    #
+    #     self.model.add(Flatten())
+    #     self.model.add(Dense(512, kernel_regularizer=l2(0.01)))
+    #     # try to change the l2
+    #     self.model.add(Activation('relu'))
+    #     self.model.add(Dropout(0.5))
+    #     self.model.add(Dense(self.num_classes))
+    #     self.model.add(Activation('softmax'))
+    def __init__(self, num_classes=10):
         self.batch_size = 32
-        self.num_classes = 10
+        self.num_classes = num_classes
         self.epochs = 50
         self.input_shape = (32, 32, 3)
 
-        # self.epochs = 5
-        # self.model = Sequential([
-        #     Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
-        #     MaxPooling2D((2, 2)),
-        #     Flatten(),
-        #     Dense(self.num_classes, activation='softmax')
-        # ])
-
         self.model = Sequential()
         self.model.add(Conv2D(64, (3, 3), padding='same',
-                              input_shape=self.input_shape))
-        self.model.add(Activation('relu'))
-        self.model.add(BatchNormalization())
-        self.model.add(Conv2D(64, (3, 3)))
-        self.model.add(Activation('relu'))
-        self.model.add(BatchNormalization())
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Dropout(0.25))
+                              input_shape=self.input_shape))  # 32*32*64
+        self.model.add(Activation('relu'))  # 32*32*64
+        self.model.add(BatchNormalization())  # 32*32*64
+        self.model.add(Conv2D(64, (3, 3)))  # 32*32*1 ->  30*30*64
+        self.model.add(Activation('relu'))  # 30*30*64
+        self.model.add(BatchNormalization())  # 30*30*64
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))  # 15*15*64
+        self.model.add(Dropout(0.25))  # 15*15*64
 
-        self.model.add(Conv2D(128, (3, 3), padding='same'))
-        self.model.add(Activation('relu'))
-        self.model.add(BatchNormalization())
-        self.model.add(Conv2D(128, (3, 3)))
-        self.model.add(Activation('relu'))
-        self.model.add(BatchNormalization())
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Dropout(0.25))
+        self.model.add(Conv2D(128, (3, 3), padding='same'))   # 15*15*1 -> 15*15*128
+        self.model.add(Activation('relu'))  # 15*15*128
+        self.model.add(BatchNormalization())  # 15*15*128
+        self.model.add(Conv2D(128, (3, 3)))  # 15*15*1 -> 13*13*128
+        self.model.add(Activation('relu'))  # 13*13*128
+        self.model.add(BatchNormalization())  # 13*13*128
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))  # 6*6*128
+        self.model.add(Dropout(0.25))  # 6*6*128
 
-        self.model.add(Flatten())
-        self.model.add(Dense(512, kernel_regularizer=l2(0.01)))
-        # try to change the l2
-        self.model.add(Activation('relu'))
-        self.model.add(Dropout(0.5))
-        self.model.add(Dense(self.num_classes))
-        self.model.add(Activation('softmax'))
+        self.model.add(Flatten())  # 4,608*1*1
+        self.model.add(Dense(512, kernel_regularizer=l2(0.001)))  # 512*1*1
+        self.model.add(Activation('relu'))  # 512*1*1
+        self.model.add(Dropout(0.5))  # 512*1*1
+        self.model.add(Dense(self.num_classes))  # 10*1*1
+        self.model.add(Activation('softmax')) # 10*1*1
 
     def train(self, x_train, y_train, x_val, y_val):
-        self.model.compile(loss='categorical_crossentropy',
-                           optimizer='sgd',
-                           # optimizer= optimizers.SGD(lr=0.01, momentum=0.9)
-                           metrics=['accuracy'])
+        # self.model.compile(loss='categorical_crossentropy',
+        #                    optimizer='sgd',
+        #                    # optimizer= optimizers.SGD(lr=0.01, momentum=0.9)
+        #                    metrics=['accuracy'])
+        optimizer = Adam(learning_rate=1e-4)
+        loss = 'categorical_crossentropy'  # SparseCategoricalCrossentropy(from_logits=False)
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+
         self.history = self.model.fit(x_train, y_train,
                                       batch_size=self.batch_size,
                                       epochs=self.epochs,
@@ -66,7 +106,6 @@ class CNN:
     def evaluate_accuracy(self, x_test, y_test):
         _, accuracy = self.model.evaluate(x_test, y_test)
         return accuracy
-
 
     def save_model(self, filepath):
         # Save the trained model to a file
@@ -90,3 +129,6 @@ class CNN:
         loaded_cnn = cls()
         loaded_cnn.history = joblib.load(filepath)
         return loaded_cnn
+
+    def summary(self):
+        self.model.summary()
--- classification_project/study/test_cnn.py
+++ classification_project/study/test_cnn.py
@@ -2,7 +2,7 @@ import pandas as pd
 from classification_project.preprocessing.preprocessing import Preprocessing
 from classification_project.models.cnn import CNN
 
-df = pd.read_csv('../../data/processed/cifar-10.csv')
+df = pd.read_csv('../../data/processed/cifar-10-100.csv')
 preprocessing = Preprocessing(df)
 preprocessing.prepare_data()
 x_train, y_train, x_val, y_val, x_test, y_test = preprocessing.split_data(one_hot_encoder=True)
@@ -10,7 +10,7 @@ x_train, y_train, x_val, y_val, x_test, y_test = preprocessing.split_data(one_ho
 # loaded_model = CNN.load_cnn_model('../saved_model/saved_cnn_model.h5')
 # loaded_history_model = CNN.load_cnn_history('../saved_model/saved_cnn_history.pkl')
 
-cnn_model = CNN()
+cnn_model = CNN(num_classes=15)
 history = cnn_model.train(x_train, y_train, x_val, y_val)
 
 cnn_model.save_model('../saved_model/saved_cnn_model.keras')
