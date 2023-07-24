e02926f68f45c205257fa233b642544bd628476e
myriam-Elkouby <essayagmyriam>
2023-07-24 18:41:45 +0300

    gui for view img from class

--- classification_project/gui/upload_img.py
+++ classification_project/gui/upload_img.py
@@ -3,6 +3,8 @@ import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
 
+from classification_project.study.use_Visualization import plot_images_to_given_label
+
 app = Dash(__name__)
 df = pd.read_csv(r'../../data/processed/cifar-10-100.csv')
 
@@ -15,21 +17,31 @@ app.layout = html.Div([
 
     html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
 
-    dcc.Dropdown(id="slct_year",
+    dcc.Dropdown(id="slct_class",
                  options=[
-                     {"label": "2015", "value": 2015},
-                     {"label": "2016", "value": 2016},
-                     {"label": "2017", "value": 2017},
-                     {"label": "2018", "value": 2018}],
+                     {"label": "airplane", "value": 0},
+                     {"label": "automobile", "value": 1},
+                     {"label": "bird", "value": 2},
+                     {"label": "cat", "value": 3},
+                     {"label": "deer", "value": 4},
+                     {"label": "dog", "value": 5},
+                     {"label": "frog", "value": 6},
+                     {"label": "horse", "value": 7},
+                     {"label": "ship", "value": 8},
+                     {"label": "truck", "value": 9},
+                     {"label": "fish", "value": 10},
+                     {"label": "people", "value": 11},
+                     {"label": "flowers", "value": 12},
+                     {"label": "trees", "value": 13},
+                     {"label": "fruit and vegetables", "value": 14}],
                  multi=False,
-                 value=2015,
-                 style={'width': "40%"}
+                 value=0,
+                 style={'width': "50%"}
                  ),
 
     html.Div(id='output_container', children=[]),
     html.Br(),
-
-    dcc.Graph(id='my_bee_map', figure={})
+    dcc.Textarea(id='my_img_map', figure={})
 
 ])
 
@@ -38,28 +50,38 @@ app.layout = html.Div([
 # Connect the Plotly graphs with Dash Components
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
-     Output(component_id='my_bee_map', component_property='figure')],
-    [Input(component_id='slct_year', component_property='value')]
+     Output(component_id='my_img_map', component_property='figure')],
+    [Input(component_id='slct_class', component_property='value')]
 )
-def update_graph(option_slctd):
+def update_view(option_slctd):
     print(option_slctd)
     print(type(option_slctd))
 
-    container = "The year chosen by user was: {}".format(option_slctd)
+    container = "The class chosen by user was: {}".format(option_slctd)
 
     dff = df.copy()
-    dff = dff[dff["Year"] == option_slctd]
-    dff = dff[dff["Affected by"] == "Varroa_mites"]
+    dff = dff[dff["label"] == option_slctd]
+    dff = dff[:9, 2:]
 
     # Plotly Express
 
-    fig = px.bar(
-        data_frame=dff,
-        x='State',
-        y='Pct of Colonies Impacted'
-    )
+    fig = px.imshow(
+        img = dff,
+        labels =dict(x='images', color= 'red'),
+        data_frame = dff,
+        facet_col_wrap = 3,
+        facet_col_spacing = 0.2,
+        facet_row_spacing = 0.2,
+        title = "image from class",
+        width = 2000,
+        height = 2000,
+        aspect = 'equal',
+        contrast_rescaling ='infer',
+        binary_format='png'
+    )#→ plotly.graph_objects._figure.Figure
+
 
     return container, fig
 
 
-app.run_server()
+# app.run_server()
