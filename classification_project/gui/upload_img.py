bbe64559548c90eca8c9eb48960e9cb87370a7ba
myriam-Elkouby <essayagmyriam>
2023-07-24 15:52:25 +0300

    gui

---
+++ classification_project/gui/upload_img.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0 or higher)
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
+df = pd.read_csv(r'../../data/processed/cifar-10-100.csv')
+
+#df = df.groupby(['label', 'is_train'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+print(df[:5])
+# ------------------------------------------------------------------------------
+# App layout
+app.layout = html.Div([
+
+    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id="slct_year",
+                 options=[
+                     {"label": "2015", "value": 2015},
+                     {"label": "2016", "value": 2016},
+                     {"label": "2017", "value": 2017},
+                     {"label": "2018", "value": 2018}],
+                 multi=False,
+                 value=2015,
+                 style={'width': "40%"}
+                 ),
+
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='my_bee_map', figure={})
+
+])
+
+
+# ------------------------------------------------------------------------------
+# Connect the Plotly graphs with Dash Components
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_year', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The year chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff["Year"] == option_slctd]
+    dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+
+    fig = px.bar(
+        data_frame=dff,
+        x='State',
+        y='Pct of Colonies Impacted'
+    )
+
+    return container, fig
+
+
+app.run_server()
--- classification_project/study/test_new_images.py
+++ classification_project/study/test_new_images.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('../../data/processed/cifar-10-100.csv')
 
+
 last_img = df.iloc[-1,:][2:].values.reshape(3,32,32).transpose(1,2,0)
 
 plt.imshow(last_img)
