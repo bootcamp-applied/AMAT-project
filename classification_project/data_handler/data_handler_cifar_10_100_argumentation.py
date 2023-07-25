438a8a7c4e19fb0795ce2f42fd084b4c8a6691ac
ruth-katzir <rutkatzir@gmail.com>
2023-07-25 23:34:05 +0300

    add df_argumentation

---
+++ classification_project/data_handler/data_handler_cifar_10_100_argumentation.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+
+class DataHandlerCifar10Cifar100:
+    def __init__(self):
+        self.cifar_10_db = None
+        self.cifar_100_db = None
+
+    def read_from_csv(self):
+        path_argumentation = '../../data/processed/argumentation.csv'
+        path_cifar_10_100='../../data/processed/cifar-10-100.csv'
+        df_cifar_10_100=pd.read_csv(path_cifar_10_100)
+        df_argumentation = pd.read_csv(path_argumentation)
+        return df_cifar_10_100,df_argumentation
+
+    # without sub class
+    def load_data_to_csv(self):
+        self.df_cifar_10_100, self.df_argumentation = self.read_from_csv()
+
+        merged_df = pd.concat([self.df_cifar_10_100, self.df_argumentation])
+        path = '../../data/processed/cifar-10-100-argumentation.csv'
+
+        if os.path.exists(path):
+            # If the file exists, delete it
+            os.remove(path)
+
+        merged_df.to_csv(path, index=False, encoding='utf-8', mode='w')
