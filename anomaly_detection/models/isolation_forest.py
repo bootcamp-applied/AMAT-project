4df2bea0f55b0bda332bc6e723583acacce9c29a
Hadas-lab <hadasofek2@gmail.com>
2023-07-26 17:37:53 +0300

    divide isolation forest to classes and train on extracted features

--- anomaly_detection/models/isolation_forest.py
+++ anomaly_detection/models/isolation_forest.py
@@ -1,18 +1,28 @@
+
+import pandas as pd
+
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+import tensorflow as tf
+
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(96, 96, 3))
+path = '../../data/processed/cifar-10-100.csv'
+df = pd.read_csv(path, dtype='int')
+images = df.iloc[:,2:].values.reshape((-1, 3, 32, 32)).transpose(0,2,3,1)
+images = tf.image.resize(images, (96, 96))
+feature = base_model.predict(preprocess_input(images))
+print('hi')
+
+
+
 from sklearn.ensemble import IsolationForest
 
-train_images = []
-test_images = []
-flattened_train_images = train_images.reshape(train_images.shape[0], -1)
-flattened_test_images = test_images.reshape(test_images.shape[0], -1)
+grouped = df.groupby('label')
+models = {}
 
-
-isolation_forest = IsolationForest(contamination=0.25)
-isolation_forest.fit(flattened_train_images)
-
-anomaly_scores = isolation_forest.score_samples(flattened_test_images)
-
-threshold = -0.5
-for i in range(len(test_images)):
-    if anomaly_scores[i] < threshold:
-        print(f'anomaly detected in image {i}')
-
+for name, data in grouped:
+  images = data.iloc[:,2:].values.reshape((-1, 3, 32, 32)).transpose(0,2,3,1)
+  images = tf.image.resize(images, (96, 96))
+  feature = base_model.predict(preprocess_input(images))
+  isolation_forest = IsolationForest(contamination=0.05)
+  isolation_forest.fit(feature.reshape(feature.shape[0],-1))
+  models[name] = isolation_forest
--- recycle bin/test_new_images.py
+++ recycle bin/test_new_images.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import cv2
 
-df = pd.read_csv('../data/processed/cifar-10-100.csv')
+df = pd.read_csv('../data/processed/cifar-10-100.csv',nrows=3)
 
-last_img = df.iloc[-1,:][2:].values.reshape(3,32,32).transpose(1,2,0)
+first_img = df.iloc[1,:][2:].values.reshape(3,32,32).transpose(1,2,0)
 
-plt.imshow(last_img)
-plt.show()
+cv2.imwrite('new_image.jpg',first_img)
