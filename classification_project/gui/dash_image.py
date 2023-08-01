7d21bcc0ceb226e8487ca1a7aa09636af19aeb5d
oryanOhayon <oryano778@gmail.com>
2023-08-01 19:50:25 +0300

    plot similar images

--- classification_project/gui/dash_image.py
+++ classification_project/gui/dash_image.py
@@ -24,22 +24,41 @@ app.layout = html.Div(
                 html.Img(id='uploaded-image', src='', style={'width': '300px'})
             ]
         ),
+        html.Div(id='similar-images-container'),
+        # html.Div(
+        #     [
+        #         html.Img(src=f"data:image/png;base64,{gui_utils.encode_image(img_vector)}",
+        #                  style={'width': '300px', 'height': '300px'})
+        #         for img_vector in similar_images
+        #     ],
+        #     style={'display': 'flex', 'flex-wrap': 'wrap', 'justify-content': 'center'}
+        # ),
         html.H2(id='image-label', style={'text-align': 'center', 'margin-top': '20px'})
     ]
 )
 
+
 @app.callback(
     [Output(component_id='image-label', component_property='children'),
-     Output(component_id='uploaded-image', component_property='src')],
+     Output(component_id='uploaded-image', component_property='src'),
+     Output(component_id='similar-images-container', component_property='children')],
     [Input(component_id='upload-data', component_property='contents')]
 )
 def classify_label(contents):
     label_mes = ''
+    similar_images_container = None
     if contents is not None:
         formated_image = gui_utils.format_image(contents)
         label = gui_utils.predict_label(formated_image)
         label_mes = f'Your image label is: {label}'
-    return label_mes, contents
+        closest_images = gui_utils.similar_images()
+        similar_images_container = [
+            html.Div([
+                html.Img(src=f"data:image/png;base64,{gui_utils.encode_image(img_vector)}",
+                         style={'width': '300px', 'height': '300px'})
+            ]) for img_vector in closest_images
+        ]
+    return label_mes, contents, similar_images_container
 
 
 if __name__ == '__main__':
--- classification_project/gui/gui_utils.py
+++ classification_project/gui/gui_utils.py
@@ -3,42 +3,80 @@ import json
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-from classification_project.models.CNN1 import CNN
+import pandas as pd
+from classification_project.models.CNN1 import CNN1
 from classification_project.utils.handling_new_image import NewImage
+import base64
+from PIL import Image
+import io
+
+new_image = None
+label = None
 
 
 def format_image(image):
+    global new_image
     _, image = image.split(',')
     image = base64.b64decode(image)
     # Convert decoded image data to numpy array
     image = np.frombuffer(image, np.uint8)
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     handler = NewImage()
-    # image, _ = handler.image_handle(image)
+    image, _ = handler.image_handle(image)
     # flat the image
     new_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # flat_img = new_image.transpose(2, 0, 1).reshape(1, -1)
-    # normalize the values
     nor_image = new_image.astype('float32') / 255
-    nor_image = nor_image.reshape((1, 32, 32, 3))  # .transpose(0, 2, 3, 1)
-    plt.imshow(new_image)
-    plt.show()
-    return nor_image
+    final_image = nor_image.reshape((1, 32, 32, 3))  # .transpose(0, 2, 3, 1)
+    return final_image
 
 
 def predict_label(image):
+    global label
     # load the model
-    loaded_model = CNN.load_cnn_model('../saved_model/saved_cnn_model.keras').model
+    loaded_model = CNN1.load_cnn_model('../saved_model/saved_cnn_model.keras').model
     probabilities = loaded_model.predict(image)
     label = np.argmax(probabilities)
-    label_category = convert_to_category(label)
+    label_category = convert_to_category()
     return label_category
 
 
-def convert_to_category(label):
+def convert_to_category():
     map_label = '../utils/dict.json'
     with open(map_label, 'r') as f:
         label_dict = json.load(f)
     label_category = next((val for key, val in label_dict.items() if key == str(label)), None)
     return label_category
 
+
+def similar_images():
+    df = pd.read_csv('../../data/processed/cifar-100.csv')
+    df_class = df[df['label'] == label]
+    features_class = df_class.iloc[:, 2:]
+    # features_array = features_class.to_numpy()
+    flattened_image = np.ravel(new_image)
+    distances = []
+    for row in features_class.values:
+        distances.append(np.sum(np.abs(row - flattened_image)))
+    sorted_indices = np.argsort(distances)
+    four_closest_indices = sorted_indices[:4]
+    four_closest_vectors = features_class.iloc[four_closest_indices]
+    reshaped_transposed_list = [row.reshape(3, 32, 32).transpose(1, 2, 0) for row in four_closest_vectors.values]
+    # four_closest_vectors = four_closest_vectors.values.reshape(3, 32, 32).transpose(1, 2, 0)
+    # closest = four_closest_vectors.values[0].reshape(3, 32, 32).transpose(1, 2, 0)
+    # plt.imshow(closest)
+    # plt.show()
+    return reshaped_transposed_list
+
+
+def encode_image(image_vector):
+    # Reshape the image_vector to (32, 32, 3) if it's in the correct shape
+    image_array = image_vector.reshape(32, 32, 3)
+    # Convert the image_array (a NumPy array) to a PIL image
+    img = Image.fromarray(image_array.astype('uint8'))
+    # Convert the PIL image to a base64-encoded string
+    buffer = io.BytesIO()
+    img.save(buffer, format="PNG")
+    base64_image = base64.b64encode(buffer.getvalue()).decode()
+    # plt.imshow(base64_image)
+    # plt.show()
+    return base64_image
