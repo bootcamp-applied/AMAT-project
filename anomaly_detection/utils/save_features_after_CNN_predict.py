2d10110bd4c39d1a75881e7ec15fd197948a135a
oryanOhayon <oryano778@gmail.com>
2023-08-27 12:56:56 +0300

    ...

--- anomaly_detection/utils/save_features_after_CNN_predict.py
+++ anomaly_detection/utils/save_features_after_CNN_predict.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
 import pandas as pd
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
-from tensorflow.image import resize
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
 
 from classification_project.models.CNN1 import CNN
 
 data = pd.read_csv('../../data/processed/cifar_10_100.csv')
+print(data.shape)
 data = data.iloc[:, 2:]
 
 # Convert the DataFrame to a NumPy array (shape: (60000, 3072))
@@ -16,7 +17,7 @@ images = data.to_numpy()
 images = images.reshape(-1, 32, 32, 3)
 
 
-model = CNN.load_cnn_model('../../classification_project/saved_model/saved_cnn_model.h5').model
+model = CNN.load_cnn_model('../../classification_project/save_models/cnn_model_1.h5').model
 preprocessed_images = preprocess_input(images)
 feature_layer_index = -2  # Index of the layer before the final dense layer
 feature_layer_output = model.layers[feature_layer_index].output
@@ -38,8 +39,15 @@ features_df.columns = features_df.columns.astype(str)
 path = '../../data/processed/features_after_CNN.feather'
 
 if os.path.exists(path):
-    # If the file exists, delete it
-    os.remove(path)
+    # os.remove(path)
+    existing_df = pd.read_feather(path)
+    print(existing_df.shape)
+    # Concatenate the existing DataFrame with the new DataFrame
+    features_df = pd.concat([existing_df, features_df], ignore_index=True)
 
 # Save the DataFrame to a Feather file
 features_df.to_feather(path)
+
+
+
+
